0b72d3414e40d68f66ff56690f13e9dfb72a4d67
yuzu0308 <yuta1538@icloud.com>
2025-03-14 14:17:56 +0900

    Initial commit with Git version info

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace VersionInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 現在のアセンブリの情報を取得
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = assembly.GetName();
+
+            // バージョン情報を取得
+            Version version = assemblyName.Version;
+
+            // バージョン情報をコンソールに表示
+            Console.WriteLine($"Assembly Version: {version}");
+
+            // Git のコミットハッシュを表示
+            string gitCommitHash = ThisAssembly.Git.Commit;
+            Console.WriteLine($"Git Commit Hash: {gitCommitHash}");
+        }
+    }
+}
